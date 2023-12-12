c7ff7667cde8c65f6a52d0d565ddf8b83022ec5e
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-12 23:54:50 +1300

    create twelve days of christmas function

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     f_to_c(temp);
     */
 
+    /* Fibonacci value finder 
     fn fibonacci(n: i32){
         let mut prev = 0;
         let mut cur = 1;
@@ -19,4 +20,20 @@ fn main() {
         println!("{cur}");
     }
     fibonacci(19)
+    */
+
+    fn twelve_days() {
+        let num_arr = ["first", "second", "third", "fourth", "fith", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
+        let quote_arr = ["A partridge in a pear tree", "Two turtle doves and", "Three french hens", "Four calling birds", "Five golden rings", "Six geese a-laying", "Seven swans a-swimming", "Eight maids a-milking", "Nine ladies dancing", "Ten lords a-leaping", "Eleven pipers piping", "Twelve drummers drumming"];
+        for n in 0..11 {
+            let num = num_arr[n];
+            println!("On the {num} day of Christmas, my true love sent to me");
+            for a in (0..(n+1)).rev() {
+                let quote = quote_arr[a];
+                println!("{quote}");
+            }
+            println!()
+        }
+    }
+    twelve_days();
 }
