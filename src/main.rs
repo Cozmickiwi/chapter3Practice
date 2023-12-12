dbd1299f3028b341b9aee81fc67d7113343de3d3
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2023-12-12 23:25:44 +1300

    make f to c converter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    
+    /* Fahrenheit to Celcius convert
+    let temp = 75;
+    fn f_to_c(t: i32) {
+        let c_temp = (t - 32) as f64 *(5.0/9.0);
+        println!("{c_temp}");
+    }
+    f_to_c(temp);
+    */
+
+    fn fibonacci(n: i32){
+        
+    }
 }
